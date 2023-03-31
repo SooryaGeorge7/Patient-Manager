c1c7fd805e2e478bc62fc7430a56ac286dad3a9c
SooryaGeorge7 <sooryageorge07@gmail.com>
2023-03-31 21:57:14 +0000

    installed and imported bcrypt

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ import datetime
 import gspread
 from google.oauth2.service_account import Credentials
 from colorama import Fore, Style
+import bcrypt
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
