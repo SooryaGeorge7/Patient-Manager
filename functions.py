d11eae18cc2b7c78b00e578c79d4e0bf2ae603df
SooryaGeorge7 <sooryageorge07@gmail.com>
2023-04-16 19:45:01 +0000

    added comments in functions.py

--- functions.py
+++ functions.py
@@ -1,6 +1,9 @@
 """
 This file contains the main classes and functions that operate this application
 """
+# Learnt how to use datetime here:
+# https://stackoverflow.com/questions/16870663/how-do-i-validate-a-date-string-format-in-python
+#  https://stackoverflow.com/questions/33076617/how-to-validate-time-format
 import re
 import datetime
 from colorama import Fore, Style
@@ -22,6 +25,9 @@ class Patient:
     menu option a or b.
     """
     def __init__(self, file_number):
+        """
+        This method initializes file number
+        """
         self.file_number = file_number
 
     def patient_details(self, file_number):
@@ -33,7 +39,9 @@ class Patient:
         displays error message.
         """
         try:
+            # Find the the row of inputed file no in patients sheet
             row = file_no_col.index(file_number) + 1
+            # Store row values in details
             details = patients.row_values(row)
             print(f"""{Fore.YELLOW}
               ---Patient Details---
@@ -63,6 +71,10 @@ class Scheduler:
     and show all appointments of a patient.
     """
     def __init__(self):
+        """
+        This method initializes a new list which will contain
+        new appointments
+        """
         self.new_appointment = []
 
     def is_available(self, date, time):
@@ -73,6 +85,8 @@ class Scheduler:
         sheet in gspread.
         """
         appointment_data = appointments.get_all_values()
+        # Use for loop to check if inputted date, time is already stored
+        # in appointments sheet
         for line in appointment_data:
             if date == line[1] and time == line[2]:
                 print(f"{Fore.RED} Sorry,That date and time is already booked")
@@ -87,15 +101,18 @@ class Scheduler:
         data sheet.
         """
         if self.is_available(date, time):
+            # Use append to add to new list
             self.new_appointment.append(file_number)
             self.new_appointment.append(date)
             self.new_appointment.append(time)
             self.new_appointment.append(reason)
             # Used for loop to check the prices in treatments sheet.
             # Use range(len())to iterate through all treatments
+            # Prices are exported to appointments sheet.
             for i in range(len(t_headings)):
                 if reason == t_headings[i]:
                     self.new_appointment.append(t_costs[i])
+            # New list is the added to appointments sheet as a new row
             appointments.append_row(self.new_appointment)
             print(f"{Fore.GREEN} Added appointment succesfully!")
 
@@ -108,6 +125,7 @@ class Scheduler:
         appropriate message to user.If inputed file number cant be found
         in data sheet , error message is shown.
         """
+        # find row of patient details with inputed file no
         pt_row = file_no_col.index(file_number) + 1
         pt_details = patients.row_values(pt_row)
         file_num = appointments.col_values(1)
@@ -118,6 +136,8 @@ class Scheduler:
         appointment_reason = appointments.col_values(4)
         appointment_price = appointments.col_values(5)
         number = 0
+        # Use for loop to check if inputed file no matches with
+        # file numbers in appointment sheet column 1
         for value in range(len(file_num)):
             if file_number == file_num[value]:
                 number += 1
@@ -135,13 +155,13 @@ def payment_due():
     Function that allows user to choose treatments to calculate
     total payment due and displays appropriate message when given
     invalid input.
+    This function is called when user chooses option d, then
+    calculate payment due in main menu.
     """
     payment = False
     # Define a new list to add all the treatments user chooses
     prices = []
     while payment is False:
-        # Get values from treatment sheet
-        # Store each row values in different variable
         addition = False
         pt_treatment = input(f"""{Fore.LIGHTYELLOW_EX}
  Please a choose treatment for patient from the following options:
@@ -156,10 +176,10 @@ def payment_due():
  Root Canal
  :\n {Style.RESET_ALL}""").lower()
         clear_terminal()
-        # Use for loop to check if input is in headings
+        # Use for loop to check if inputted treatment is in t_headings
         for title in t_headings:
-
             if pt_treatment == title:
+                # Allow user to add another or view total due
                 u_choice = input(f"""{Fore.LIGHTYELLOW_EX}
  Choose between
  a) Add another treatment
@@ -173,22 +193,22 @@ def payment_due():
 
                 elif u_choice == "b":
                     payment = True
-                    # Last input is also added to prices list
+                    # Last input is added to prices list
                     prices.append(pt_treatment)
                     addition = True
-                    # Change values in costs to integer with int()
+                    # Change values of t_costs to integer with int()
                     # Store integers in new list
                     int_costs = [int(x) for x in t_costs]
                     i = 0
                     # Define a new list to add the chosen treatment's prices
                     final_list = []
-                    # Use for loop to check if item in price is in headings
+                    # Use for loop to check if item in price is in t_headings
                     for i in prices:
                         if i in t_headings:
                             #  Use index() to check the treatment price
                             #  Append it to final_list
                             final_list.append(int_costs[t_headings.index(i)])
-                    # Use sum() to add integers in list
+                    # Use sum() to add integers in final_list
                     print(f"{Fore.YELLOW} Total payment due"
                           f" is {sum(final_list)}")
                     # The break statement will return patient back to menu
@@ -206,6 +226,8 @@ def view_treatments():
     """
     Function that displays the treatments and corresponding prices in
     datasheet treatments.
+    This function is called when user chooses option d and then
+    view prices in main menu.
     """
     i = 0
     print(f"{Fore.YELLOW} Treatment Prices")
@@ -218,6 +240,7 @@ def validate_email():
     """
     Function that validates user's inputed email by using regular
     expressions to match pattern.
+    This function is called when user chooses to add patients in main menu.
     """
     # Learnt about pattern matching and regular expression here
     # https://www.geeksforgeeks.org/pattern-matching-python-regex/
@@ -239,6 +262,7 @@ def validate_app_date():
     Function that takes in appointment date from user and checks if date is
     valid using datetime. It also checks if inputted date is in the
     future, and displays error message if not.
+    This function is called when user chooses to add appointment in main menu.
     """
     # Learnt about date time to validate dates here
     # https://stackoverflow.com/questions/16870663/how-do-i-validate-a-date-string-format-in-python
@@ -266,6 +290,7 @@ def validate_birthdate():
     Function that validates DOB entered by user by checking if its
     a date in the past. It also checks if the user has inputted a date
     in the correct format.
+    This function is called when user chooses to add patients in main menu.
     """
     while True:
         try:
@@ -287,6 +312,7 @@ def validate_birthdate():
 def validate_time():
     """
     Function that validates appointment time user inputs using date time.
+    This function is called when user chooses to add appointment in main menu
     """
     # Learnt how to validate time using date time here
     # https://stackoverflow.com/questions/33076617/how-to-validate-time-format
@@ -306,6 +332,7 @@ def validate_fileno():
     inputed file number is already in patients datasheet so that user
     doesnt add the same patient twice. If the file number is already been used,
     user is given approriate message and returned back to the menu.
+    This function is called when user chooses option b in main menu
     """
     patient_data = patients.get_all_values()
     while True:
@@ -330,7 +357,8 @@ def validate_fileno():
 def validate_treatment():
     """
     Function that validates inputed treatment to check if it is
-    a treatment provided by the clinic.
+    a treatment provided by the clinic. This is called when
+    user chooses option c- and add appointment
     """
     validate = False
     while validate is False:
