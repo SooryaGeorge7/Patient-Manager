9dff55d961230a146eff64792a5a8dfeecc575fe
SooryaGeorge7 <sooryageorge07@gmail.com>
2023-04-16 18:56:46 +0000

    added comments in google_sheets_api.py

--- google_sheets_api.py
+++ google_sheets_api.py
@@ -1,7 +1,7 @@
 """
 This is the file that contains google sheet API connections
 """
-
+# Import data from google sheets
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -12,12 +12,15 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
+# Creds.json is added to gitignore
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Patient_manager')
+
+# The sheets from google sheets that are used by app
 users = SHEET.worksheet('users')
 patients = SHEET.worksheet('patients')
 appointments = SHEET.worksheet('appointments')
 treatments = SHEET.worksheet('treatments')
-treatments_data = treatments.get_all_values()
+# treatments_data = treatments.get_all_values()
